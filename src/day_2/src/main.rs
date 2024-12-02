6a94f8eb444f82fef1d4944e9064dbbb83cfd382
Walter <wrstocke@googlemail.com>
2024-12-02 08:48:32 +0100

    feat(day_2): part 1 solved of puzzle

--- src/day_2/src/main.rs
+++ src/day_2/src/main.rs
@@ -1,7 +1,16 @@
 mod parser;
 
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let puzzle_input = aoc_core::get_input(2024, 2)?;
+
+    let reports = parser::parse_reports(&puzzle_input);
+    let number_of_safe_reports = count_safe_reports(reports);
+
+    println!("Number of safe reports: {}", number_of_safe_reports);
+
+    Ok(())
 }
 
 fn count_safe_reports(reports: Vec<Vec<i32>>) -> usize {
