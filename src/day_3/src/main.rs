3662df15d63d868b049d89d9381b5b55aabf2aef
Walter <wrstocke@googlemail.com>
2024-12-03 13:27:40 +0100

    feat(day_3): part 2 solved

--- src/day_3/src/main.rs
+++ src/day_3/src/main.rs
@@ -11,6 +11,10 @@ fn main() -> Result<()> {
 
     println!("The sum of all uncorrupted mul instructions in the input is: {}", sum);
 
+    let sum = sum_mul(&puzzle_input);
+
+    println!("The sum of all uncorrupted mul instructions with enabler in the input is: {}", sum);
+
     Ok(())
 }
 
@@ -22,7 +26,7 @@ fn sum_uncorrupted_mul_instructions(puzzle_input: &str) -> i32 {
         .sum()
 }
 
-fn sum(puzzle_input: &str) -> i32 {
+fn sum_mul(puzzle_input: &str) -> i32 {
     let instructions = instructions_parser::parse(puzzle_input).unwrap();
 
     let mut sum = 0;
@@ -46,7 +50,7 @@ fn sum(puzzle_input: &str) -> i32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::{sum, sum_uncorrupted_mul_instructions};
+    use crate::{sum_mul, sum_uncorrupted_mul_instructions};
 
     const TEST_DATA: &str = r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
@@ -58,7 +62,7 @@ mod tests {
     const TEST_DATA_2: &str = r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
-    fn sum_should_return_48_for_test_data() {
-        assert_eq!(48, sum(TEST_DATA_2));
+    fn sum_mul_should_return_48_for_test_data() {
+        assert_eq!(48, sum_mul(TEST_DATA_2));
     }
 }
