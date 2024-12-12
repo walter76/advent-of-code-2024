61bb8e40ac8f74e4461d2b489dfc7fbca393fa82
Walter <wrstocke@googlemail.com>
2024-12-12 20:38:22 +0100

    refactor(day_5): code cleanup in main.rs

--- src/day_5/src/main.rs
+++ src/day_5/src/main.rs
@@ -8,25 +8,23 @@ use rule::PageOrderingRule;
 fn main() -> Result<()> {
     let puzzle_input = aoc_core::get_input(2024, 5)?;
 
-    let result = solve(&puzzle_input);
+    let sum_of_middle_page_numbers =
+        sum_middle_page_numbers_of_valid_page_updates(&puzzle_input);
 
-    println!("{}", result);
+    println!(
+        "The sum of middle page numbers of valid page updates is: {}",
+        sum_of_middle_page_numbers);
 
     Ok(())
 }
 
-fn solve(puzzle_input: &str) -> i32 {
+fn sum_middle_page_numbers_of_valid_page_updates(puzzle_input: &str) -> i32 {
     let (page_ordering_rules, all_page_updates) = parse_input(puzzle_input);
 
-    let mut result = 0;
-
-    for page_update in all_page_updates.iter() {
-        if verify(page_update, &page_ordering_rules) {
-            result += get_middle_page_number(page_update);
-        }
-    }
-
-    result
+    all_page_updates.iter()
+        .filter(|page_update| verify_safety_manual_update(page_update, &page_ordering_rules))
+        .map(|page_update| get_middle_page_number(page_update))
+        .sum()
 }
 
 fn get_middle_page_number(page_updates: &[i32]) -> i32 {
@@ -35,7 +33,7 @@ fn get_middle_page_number(page_updates: &[i32]) -> i32 {
     page_updates[index]
 }
 
-fn verify(safety_manual_update: &[i32], page_ordering_rules: &[PageOrderingRule]) -> bool {
+fn verify_safety_manual_update(safety_manual_update: &[i32], page_ordering_rules: &[PageOrderingRule]) -> bool {
     for page_ordering_rule in page_ordering_rules.iter() {
         if !verify_page_ordering_rule(safety_manual_update, page_ordering_rule) {
             return false;
@@ -59,7 +57,7 @@ fn verify_page_ordering_rule(safety_manual_update: &[i32], page_ordering_rule: &
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_middle_page_number, parser::parse_input, solve, verify};
+    use crate::{get_middle_page_number, parser::parse_input, sum_middle_page_numbers_of_valid_page_updates, verify_safety_manual_update};
 
     const EXAMPLE_DATA: &str = r"47|53
 97|13
@@ -91,45 +89,45 @@ mod tests {
 97,13,75,29,47";
 
     #[test]
-    fn verify_should_return_true_for_first_updates_in_example_data() {
+    fn verify_safety_manual_update_should_return_true_for_first_updates_in_example_data() {
         let (page_ordering_rules, all_page_updates) = parse_input(EXAMPLE_DATA);
 
-        assert!(verify(&all_page_updates[0], &page_ordering_rules));
+        assert!(verify_safety_manual_update(&all_page_updates[0], &page_ordering_rules));
     }
 
     #[test]
-    fn verify_should_return_true_for_second_updates_in_example_data() {
+    fn verify_safety_manual_update_should_return_true_for_second_updates_in_example_data() {
         let (page_ordering_rules, all_page_updates) = parse_input(EXAMPLE_DATA);
 
-        assert!(verify(&all_page_updates[1], &page_ordering_rules));
+        assert!(verify_safety_manual_update(&all_page_updates[1], &page_ordering_rules));
     }
 
     #[test]
-    fn verify_should_return_true_for_third_updates_in_example_data() {
+    fn verify_safety_manual_update_should_return_true_for_third_updates_in_example_data() {
         let (page_ordering_rules, all_page_updates) = parse_input(EXAMPLE_DATA);
 
-        assert!(verify(&all_page_updates[2], &page_ordering_rules));
+        assert!(verify_safety_manual_update(&all_page_updates[2], &page_ordering_rules));
     }
 
     #[test]
-    fn verify_should_return_false_for_fourth_updates_in_example_data() {
+    fn verify_safety_manual_update_should_return_false_for_fourth_updates_in_example_data() {
         let (page_ordering_rules, all_page_updates) = parse_input(EXAMPLE_DATA);
 
-        assert!(!verify(&all_page_updates[3], &page_ordering_rules));
+        assert!(!verify_safety_manual_update(&all_page_updates[3], &page_ordering_rules));
     }
 
     #[test]
-    fn verify_should_return_false_for_fifth_updates_in_example_data() {
+    fn verify_safety_manual_update_should_return_false_for_fifth_updates_in_example_data() {
         let (page_ordering_rules, all_page_updates) = parse_input(EXAMPLE_DATA);
 
-        assert!(!verify(&all_page_updates[4], &page_ordering_rules));
+        assert!(!verify_safety_manual_update(&all_page_updates[4], &page_ordering_rules));
     }
 
     #[test]
-    fn verify_should_return_false_for_sixth_updates_in_example_data() {
+    fn verify_safety_manual_update_should_return_false_for_sixth_updates_in_example_data() {
         let (page_ordering_rules, all_page_updates) = parse_input(EXAMPLE_DATA);
 
-        assert!(!verify(&all_page_updates[5], &page_ordering_rules));
+        assert!(!verify_safety_manual_update(&all_page_updates[5], &page_ordering_rules));
     }
 
     #[test]
@@ -138,7 +136,7 @@ mod tests {
     }
 
     #[test]
-    fn solve_should_return_143_for_example_data() {
-        assert_eq!(143, solve(EXAMPLE_DATA));
+    fn sum_middle_page_numbers_of_valid_page_updates_should_return_143_for_example_data() {
+        assert_eq!(143, sum_middle_page_numbers_of_valid_page_updates(EXAMPLE_DATA));
     }
 }
